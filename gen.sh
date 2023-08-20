a575eabcaa9d74e299dae3b7cde1ba74578a1438
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2023-08-20 14:54:56 +0100

    fix site name

--- gen.sh
+++ gen.sh
@@ -2,4 +2,4 @@
 set -e
 
 EVENTS="Today - Market" envsubst < index.md > index.md.out
-pandoc -H head.html --metadata title="Macc.Life" -s index.md.out -o index.html
+pandoc -H head.html --metadata title="Macc.Today" -s index.md.out -o index.html
